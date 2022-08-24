8e4dfd4a6f3bb7fd8574a5af149a7b28a045b42c
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-08-24 15:08:08 -0300

    feat: Theme Changer Page added

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import { ChangeEvent, useState } from 'react';
+import {
+  Card,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
 import { Layout } from '../components/layouts';
 
 const ThemeChangerPage = () => {
+  const [currentTheme, setCurrentTheme] = useState('light');
+
+  const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCurrentTheme(event.target.value);
+  };
+
   return (
     <Layout>
-      <h1>Theme Changer Page</h1>
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>Tema</FormLabel>
+            <RadioGroup value={currentTheme} onChange={onThemeChange}>
+              <FormControlLabel
+                value='light'
+                control={<Radio />}
+                label='Light'
+              />
+              <FormControlLabel value='dark' control={<Radio />} label='Dark' />
+              <FormControlLabel
+                value='custom'
+                control={<Radio />}
+                label='Custom'
+              />
+            </RadioGroup>
+          </FormControl>
+        </CardContent>
+      </Card>
     </Layout>
   );
 };
